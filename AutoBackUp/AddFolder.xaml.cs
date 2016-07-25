4bdbf20f147714210807e4ecc5fbce921cebe6e7
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2016-07-25 10:58:13 +0100

    switched to WPF, added main, new config and new folder binding UI pages

---
+++ AutoBackUp/AddFolder.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoBackUp
+{
+    /// <summary>
+    /// Interaction logic for AddFolder.xaml
+    /// </summary>
+    public partial class AddFolder : Window
+    {
+        public AddFolder()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txt_source_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AutoBackUp/NewConfiguration.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoBackUp
+{
+    /// <summary>
+    /// Interaction logic for NewConfiguration.xaml
+    /// </summary>
+    public partial class NewConfiguration : Window
+    {
+        public NewConfiguration()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_folder_Click(object sender, RoutedEventArgs e)
+        {
+            AddFolder newFolderConfig = new AddFolder();
+            newFolderConfig.ShowDialog();
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
