3bc29c77dac36d9b75957be7e8d527cd4ebe337b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-23 13:46:25 +0100

    chore: readd gui folder

---
+++ Gui/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+using GUI;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void AddNewDrive(object sender, EventArgs e)
+    {
+        AddDrive addDrive = new AddDrive();
+        addDrive.ShowAll();
+    }
+}
